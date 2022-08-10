60dc4a92c7e6eacfbeae2171f189d532d888dae4
ValenBentos <88683261+ValenBentos@users.noreply.github.com>
2022-08-10 09:56:41 -0300

    Update Variantetablero4enraya.cpp

--- Variantetablero4enraya.cpp
+++ Variantetablero4enraya.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main(){
 char c = 'A';
+char b = '1';
 for(int i = 0; i<7; i++){
     cout<<" ";
     for(int j = 0; j<3; j++){
@@ -18,11 +19,24 @@ for(int i = 0; i<7; i++){
 }
 cout<<endl;
 for(int i = 1; i<=12; i++){
+	if(i==1 || i==3 || i==5 || i==7 || i==9 || i==11) {
+		cout<<b;
+       	b++;
+		
+}
+else {
+	cout<<" ";
+}
+	    
     for(int j = 0; j<8; j++){
+    	
         cout<<"|";
+        
         if(i%2==0 && i>1 && j<7 ){
+
             for(int jj = 0; jj<3; jj++){
                 cout<<"-";
+                
             }
         }else{
             for(int jj = 0; jj<3; jj++){
@@ -35,4 +49,3 @@ for(int i = 1; i<=12; i++){
     
 return 0;
 }
-
